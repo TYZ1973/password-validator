eb3fdf4d36e8adf7946e13e85cb4e1f519ad6aee
TYZ1973 <zangtianyi@live.com>
2018-02-10 00:55:36 -0400

    Validator java file

---
+++ Validator.java
@@ -0,0 +1,15 @@
+public class Validator{
+
+	public boolean teemo(String lol){
+	
+	
+	if(lol.equals("password")){
+	return false;
+	}
+
+	else if(lol.length()>=8){
+	return true;
+      }		
+     }
+   }	
+
